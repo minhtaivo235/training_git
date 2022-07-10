57d4769b1e21d74e8ebce8cc76230a1266276314
Tai Vo <TaiVM2@fpt.com.vn>
2022-07-10 23:02:42 +0700

    fix conflict

---
+++ script.js
@@ -0,0 +1,12 @@
+function name() {
+  const array = [1, 0, "", false, , null, undefined, "Phúc TMA"];
+  const result = array.filter((i) => i);
+  console.log(result);
+}
+name();
+
+function a() {
+  const array = [1, 0, "", false, , null, undefined, "Phúc TMA"];
+  const result = array.filter((i) => i);
+  console.log(result);
+}
